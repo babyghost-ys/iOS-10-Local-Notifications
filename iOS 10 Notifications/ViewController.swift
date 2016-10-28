0c95e20a19f32fd734323236e820740492bd1809
Peter Leung <babyghost@gmail.com>
2016-10-28 20:09:32 +0100

    initial UI setup and request permissions

--- iOS 10 Notifications/ViewController.swift
+++ iOS 10 Notifications/ViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound], completionHandler: {(got, error) in
+            
+            if got {
+                print("Access granted")
+            } else {
+                
+            }
+        })
+    }
+    
+    @IBAction func setLocalNotifications(_ sender: Any) {
     }
 
     override func didReceiveMemoryWarning() {
